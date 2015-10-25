2e40c8fffa4cee52d5c3de1ad755087f103831c2
ansarimofid <ansarimofid@gmail.com>
2015-10-25 17:29:06 +0530

    Multiple Color Option using Color Picker

---
+++ js/init.js
@@ -0,0 +1,24 @@
+$('document').ready(function () {
+    $('#picker li a').click(function () {
+        var ch = $(this).attr('data-color');
+        console.log(ch);
+        changeColor(ch);
+    });
+
+    $('.picker-toggle').click(function () {
+        $('.color-picker').toggleClass('hide-picker');
+    });
+    var pcolor = ['orange', 'pink', 'cyan', 'green', 'blue'];
+    changeColor(pcolor[Math.floor(Math.random() * 5)]);
+
+    function changeColor(ch) {
+        var color = {
+            orange: "rgb(255,61,0)",
+            pink: "rgb(233, 30, 99)",
+            cyan: "rgb(0, 150, 136)",
+            green: "rgb(76, 175, 80)",
+            blue: "rgb(33, 150, 243)"
+        };
+        $('style.dynamic-style').html(".primary-color{color: " + color[ch] + "!important;}.primary-background{background-color: " + color[ch] + " !important;}nav>ul>li>a:hover{color:" + color[ch] + "!important;}");
+    }
+})
